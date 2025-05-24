693bc316020a308a804bf65e00410472673d9a31
kado.kado112 <108268013+kado-kado@users.noreply.github.com>
2025-05-24 16:42:56 +0900

    up

--- setup/service-worker.js
+++ setup/service-worker.js
@@ -1,6 +1,14 @@
-window.onload = function() {
+  let deferredPrompt;
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    document.getElementById('installBtn').style.display = 'block';
+  });
+
+  document.getElementById('installBtn').addEventListener('click', () => {
     if (deferredPrompt) {
-      deferredPrompt.prompt();
+      deferredPrompt.prompt(); // プロンプトを表示
       deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
           console.log('ユーザーがインストールを承諾しました');
@@ -10,4 +18,4 @@ window.onload = function() {
         deferredPrompt = null;
       });
     }
-  }
+  });
